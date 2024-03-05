a0227d5473a9a741787badc3ce59fc1570ff2e4c
bharathane <mandadurgabharath@gmail.com>
2024-03-05 17:33:03 +0530

    update nodemailer service

--- app.js
+++ app.js
@@ -4,12 +4,20 @@ const sqlite3 = require("sqlite3");
 const path = require("path");
 const dbPath = path.join(__dirname, "eventsData.db");
 const cors = require("cors");
+const nodemailer = require("nodemailer");
 const app = express();
 app.use(cors());
 app.use(express.json());
 
 let db;
 
+const transporter = nodemailer.createTransport({
+  service: "gmail",
+  auth: {
+    user: "mandabharath49@gmail.com",
+    pass: "ozpp dhlr zhuz uqez",
+  },
+});
 // Establish connection between db and server
 const initailizeDbAndServer = async () => {
   try {
@@ -36,7 +44,32 @@ app.post("/postEvents/", async (req, res) => {
     const sqlQueryForPost = `insert into events(name,email,phone,eventType)
     values('${username}','${email}','${phone}','${eventType}')`;
     const dbRes = await db.run(sqlQueryForPost);
+
+    const mailOptions = {
+      from: "mandabharath49@gmail.com",
+      to: email,
+      subject: "Congractulations| Registration Successfull",
+      text: `you registration is successfull. you registration id is ${dbRes.id}`,
+    };
+
+    transporter.sendMail(mailOptions, (error, info) => {
+      if (error) {
+        return res.status(500).json({ error: error.message });
+      }
+
+      res.status(200).json({ message: "Email sent", info });
+    });
     res.send({ succesMessage: "Booking Successfull" });
+  } catch (error) {
+    console.log(error.message);
+  }
+});
+
+app.get("/getEvents", async (req, res) => {
+  try {
+    const getSqlquary = `select * from events`;
+    const dbResponse = await db.all(getSqlquary);
+    res.send({ eventsData: dbResponse });
   } catch (error) {
     console.log(error.message);
   }
