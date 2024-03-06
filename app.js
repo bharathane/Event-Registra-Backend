ebe7e2820efc3164be228621796615437128a827
bharathane <mandadurgabharath@gmail.com>
2024-03-06 12:54:34 +0530

    update email template

--- app.js
+++ app.js
@@ -44,12 +44,30 @@ app.post("/postEvents/", async (req, res) => {
     const sqlQueryForPost = `insert into events(name,email,phone,eventType)
     values('${username}','${email}','${phone}','${eventType}')`;
     const dbRes = await db.run(sqlQueryForPost);
+    const getIdQuery = `select * from events order by id desc limit 1`;
+    const getIdForDbRes = await db.all(getIdQuery);
 
     const mailOptions = {
       from: "mandabharath49@gmail.com",
       to: email,
-      subject: "Congractulations| Registration Successfull",
-      text: `your registration for  ${eventType} is successfull`,
+      subject: `Congractulations ${username} ||  Confirmation of Your Event Registration`,
+      html: `
+    <p>Dear ${username},</p>
+    <p>We hope this email finds you well. We are delighted to inform you that your registration for the upcoming ${eventType} has been successfully received and processed. We appreciate your interest in joining us for this exciting event.</p>
+    <p><strong>Event Details:</strong></p>
+
+    <p><strong>Your registration details:</strong></p>
+    <ul>
+      <li>Name: ${username}</li>
+      <li>Email: ${email}</li>
+      <li>Contact Number: ${phone}</li>
+      <li>Registration ID: ${getIdForDbRes[0].id}</li>
+    </ul>
+    <p>Please make sure to bring a valid ID and the confirmation email with your Registration ID to facilitate a smooth check-in process on the day of the event.</p>
+    <p>If you have any questions or need further assistance, feel free to reach out to our event coordinator at mandabharath49@gmail.com or 8247360108.</p>
+    <p>We are looking forward to your participation and hope you have a rewarding and enjoyable experience at ${eventType}. Thank you for being a part of this event!</p>
+    <p>Best regards,<br>Eligere Technologies<br>Venkata Durga Bharath Manda<br>8247360108</p>
+  `,
     };
 
     transporter.sendMail(mailOptions, (error, info) => {
